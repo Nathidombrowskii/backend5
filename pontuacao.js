aa0f819c3f147fd34a4b8d866559c8bd82460280
Nathidombrowskii <nathielly.prado@escola.pr.gov.br>
2026-04-07 11:52:45 +0000

    versao1

---
+++ pontuacao.js
@@ -0,0 +1,12 @@
+//Sistema de Pontuação de um jogo
+const nomeJogador = "Babi";
+let faseAtual = 3;
+let vidasRestantes = 2;
+
+let pontosPorMissao = [85, 90, 70, 100];
+
+//pontosPorMissao.length
+let pontuacaoTotal = 0;
+
+console.log(`Seja bem vinda ${nomeJogador}! `);
+console.log(pontosPorMissao);
